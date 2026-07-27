a8f519078b44cbbd1afc73a9c6a9e9928b2d58b7
Mebrie-Awoke <mebrieawoke941@gmail.com>
2026-07-27 10:45:05 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory     
+from flask import Flask, request, jsonify, render_template, send_from_directory    
 import pandas as pd
 import pickle   
 import os    

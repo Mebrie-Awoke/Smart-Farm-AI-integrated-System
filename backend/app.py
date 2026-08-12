496d8a276fc89c9f4b25766e4ee63af69f83d5c3
Mebrie Awoke <mebrieawoke941@gmail.com>
2026-08-12 13:58:52 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
       
 app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend")
 CORS(app)
-
+ 
 # Image directory - adjust path based on project structure
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
 IMAGE_DIR = os.path.join(BASE_DIR, "Images")
